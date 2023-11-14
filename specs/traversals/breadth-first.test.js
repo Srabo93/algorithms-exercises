f7a89a9ecff062bed96044cf1918dd321ceffc54
Srabo93 <a.s-dev@web.de>
2023-11-14 15:15:02 +0100

    tree traversals

--- specs/traversals/breadth-first.test.js
+++ specs/traversals/breadth-first.test.js
@@ -1,10 +1,28 @@
 const breadthFirstTraverse = (queue, array) => {
-  // fill code in here
+  if (!queue.length) return array;
+
+  const node = queue.shift();
+  array.push(node.value);
+
+  if (node.left) queue.push(node.left);
+  if (node.right) queue.push(node.right);
+
+  return breadthFirstTraverse(queue, array);
+
+  // while (queue.length) {
+  //   const node = queue.shift();
+  //   array.push(node.value);
+  //
+  //   if (node.left) queue.push(node.left);
+  //   if (node.right) queue.push(node.right);
+  // }
+  //
+  // return array;
 };
 
 // unit tests
 // do not modify the below code
-describe.skip("breadth-first tree traversal", function () {
+describe("breadth-first tree traversal", function () {
   const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
   const tree = {
@@ -16,9 +34,9 @@ describe.skip("breadth-first tree traversal", function () {
         left: {
           value: "G",
           left: null,
-          right: null
+          right: null,
         },
-        right: null
+        right: null,
       },
       right: {
         value: "E",
@@ -28,10 +46,10 @@ describe.skip("breadth-first tree traversal", function () {
           left: {
             value: "K",
             left: null,
-            right: null
-          }
-        }
-      }
+            right: null,
+          },
+        },
+      },
     },
     right: {
       value: "C",
@@ -40,16 +58,16 @@ describe.skip("breadth-first tree traversal", function () {
         left: {
           value: "I",
           left: null,
-          right: null
+          right: null,
         },
         right: {
           value: "J",
           left: null,
-          right: null
-        }
+          right: null,
+        },
       },
-      right: null
-    }
+      right: null,
+    },
   };
 
   test("breadthFirstTraverse", () => {
